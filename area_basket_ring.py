406f2d6ee17d20fca379475424d47db7755f92f0
abelkifle1216 <abelkifle1216@gmail.com>
2025-08-23 02:01:11 -0700

    done by abel

---
+++ area_basket_ring.py
@@ -0,0 +1,8 @@
+import Area
+print("rectangle area",Area.rectangle(15,5))
+print("square area",Area.square(5))
+print("circle area",Area.ellipies(5,5))
+print("tripizoid area",Area.tripizoid(5,5,6))
+print("tri area",Area.tri(5,5,5))
+
+from Area import square,rectangle,tri,circle,tripizoid,ellipies
